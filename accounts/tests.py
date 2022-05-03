3a458cdebee0f60db1b0d8ae3a7b7c082f6d7283
chiha7777 <chiharu_7777@docomo.ne.jp>
2022-05-03 22:11:34 +0900

    changed tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -7,19 +7,17 @@ from django.db.utils import IntegrityError
 from django.urls import resolve, reverse
 
 
-from .views import singup
-
 User = get_user_model()
 
 class SignUpTests(TestCase):
   def setUp(self):
-    self.url = reverse('accounts:singup')
+    self.url = reverse('accounts:signup')
   
   def test_success_signup_get(self):
     response = self.client.get(self.url)
     self.assertEquals(response.status_code, 200)
 
-  def test_successful_signup_post(sefl):
+  def test_successful_signup_post(self):
     data = {
       'username' : 'new_user',
       'password1': 'testpass1',
@@ -27,33 +25,28 @@ class SignUpTests(TestCase):
     }
 
     post_response = self.client.post(self.url, data=data)
-    mypostlist_url = reverse('tweet:mypostlist')
-    self.assertRedirects(post_response, mypostlist_url)
     self.assertTrue(User.objects.exists())
-    get_response = self.client.get(mypostlist_url)
-    user = get_response.context.get('user')
-    self.assertTrue(user.is_authenticated)
-
+    
   def test_invalid_signup_post(self):
     response = self.client.post(self.url,{})
-    self.assertEquals(response.status_cade, 200)
+    self.assertEquals(response.status_code, 200)
     form = response.context.get('form')
     self.assertTrue(form.errors)
-    self.assertFalse(User.objects.exits())
+    self.assertFalse(User.objects.exists())
 
   def test_failure_post_with_password_too_short_password(self):
     response = self.client.post(self.url, {'username' : 'new_user', 'password1' : 'jpo34', 'password2' : 'jpo34'})
-    self.assertFormError(response,'form', 'password2', 'このパスワードは短すぎます。８文字以上で入力してください。')
+    self.assertFormError(response, 'form', 'password2', 'このパスワードは短すぎます。８文字以上で入力してください。')
 
   def test_failure_post_with_password_similar_to_username(self):
     response = self.client.post(self.url, {'username' : 'new_user', 'password1' : 'new_user', 'password2' : 'new_user'})
-    self.asserFormError(response, 'form', 'password2', 'ユーザーネームとパスワードが違うものにしてください。')
+    self.assertFormError(response, 'form', 'password2', 'ユーザーネームとパスワードが違うものにしてください。')
 
   def test_failure_post_with_only_numbers_password(self):
     response = self.client.post(self.url, {'username' : 'new_user', 'password1' : '57892709', 'password2' : '57892709'})
-    self.asserFormError(response, 'form', 'password2', 'パスワードは英語と数字を組み合わせてください。')
+    self.assertFormError(response, 'form', 'password2', 'パスワードは英語と数字を組み合わせてください。')
   
   def test_failure_post_with_mismatch_password(self):
     response = self.client.post(self.url, {'username' : 'new_user', 'password1' : 'testpass1', 'password2' : 'testpass2'})
-    self.asserFormError(response, 'form', '確認用パスワードが一致しません。')
+    self.assertFormError(response, 'form', 'password2', '確認用パスワードが一致しません。')
 
