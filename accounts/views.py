1097373590400d5cabfd3863e5f8ba9386b26411
chiha7777 <chiharu_7777@docomo.ne.jp>
2022-09-06 22:45:12 +0900

    tweet

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,21 @@
 from django.views.generic import TemplateView, CreateView, ListView
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
-<<<<<<< HEAD
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 
 from .forms import SignUpForm, LoginForm
 from tweet.models import Tweet
-=======
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import(LoginView, LogoutView)
-
-from .forms import SignUpForm
-from .forms import LoginForm
->>>>>>> parent of 392aa56 (changed test.py and views.py)
 
 User = get_user_model()
+
 
 class IndexView(TemplateView):
     template_name = "accounts/index.html"
 
-<<<<<<< HEAD
 
 class HomeView(LoginRequiredMixin, ListView):
-=======
-class HomeView(TemplateView):
->>>>>>> parent of 392aa56 (changed test.py and views.py)
     template_name = "accounts/home.html"
     context_object_name = "tweet_list"
 
@@ -36,6 +25,7 @@ class HomeView(TemplateView):
     def tweet_detail(request, pk):
         tweet = get_object_or_404(Tweet, pk=pk)
         return render(request, "tweet/tweet_detail.html", {"tweet": tweet})
+
 
 class SignUpView(CreateView):
     model = User
@@ -48,14 +38,11 @@ class SignUpView(CreateView):
         self.object = user
         return super().form_valid(form)
 
-<<<<<<< HEAD
 
 class UserLoginView(LoginView):
-=======
-class Login(LoginView):
->>>>>>> parent of 392aa56 (changed test.py and views.py)
     form_class = LoginForm
-    template_name = 'accounts/login.html'
+    template_name = "accounts/login.html"
+
 
 class UserLogoutView(LogoutView):
     pass
